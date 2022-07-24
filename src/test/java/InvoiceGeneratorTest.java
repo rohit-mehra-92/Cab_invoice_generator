9ee97a286e08733c3a0b5ce0bf6362b4f8983781
Rohit Singh Mehra <rohit264cool@gmail.com>
2022-07-24 19:05:24 +0530

    Written test code for min fare at given distance and time

--- src/test/java/InvoiceGeneratorTest.java
+++ src/test/java/InvoiceGeneratorTest.java
@@ -10,4 +10,13 @@ public class InvoiceGeneratorTest {
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assertions.assertEquals(25, fare, 0.0);
     }
+
+    @Test
+    void givenDistanceAndTime_shouldReturnMinFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(5, fare, 0.0);
+    }
 }
