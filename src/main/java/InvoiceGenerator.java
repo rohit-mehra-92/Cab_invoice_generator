d5cb54d553d210fd4275974409eb5d55a606f65d
Rohit Singh Mehra <rohit264cool@gmail.com>
2022-07-24 19:11:26 +0530

    Added:Write code on main class to make pass test for minimum fare

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -1,9 +1,12 @@
 public class InvoiceGenerator {
     private static final int COST_PER_TIME = 1;
     private static final double MIN_COST_PER_KILOMETER = 10;
+    private static final double MIN_FAIR = 5;
 
     double calculateFare(double distance, int time) {
         double totalFare = distance * MIN_COST_PER_KILOMETER + time * COST_PER_TIME ;
+        if(totalFare<MIN_FAIR)
+            return MIN_FAIR;
         return totalFare;
     }
 }
